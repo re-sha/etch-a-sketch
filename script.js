6a7d9bb2473561988946cb74bda1e3139ed60ce3
re-sha <163538829+re-sha@users.noreply.github.com>
2024-05-31 00:41:07 +0530

    Add script functionality

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+const container = document.querySelector("#container");
+for (let i = 0; i < 256 ; i++) {
+        const newCell = document.createElement("div");
+        newCell.classList.add("cell");
+        container.appendChild(newCell);
+}
