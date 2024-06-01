4615bc2f9abb49d69818f9b2c8451607da6bacb5
re-sha <163538829+re-sha@users.noreply.github.com>
2024-06-02 04:15:38 +0530

    create grids

--- script.js
+++ script.js
@@ -2,5 +2,8 @@ const container = document.querySelector("#container");
 for (let i = 0; i < 256 ; i++) {
         const newCell = document.createElement("div");
         newCell.classList.add("cell");
+        newCell.addEventListener("mouseover", () => {
+                newCell.style.background = "pink";
+        })
         container.appendChild(newCell);
 }
