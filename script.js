0d8b3d8855a310f41421da30d505a9dbc0c3a879
re-sha <163538829+re-sha@users.noreply.github.com>
2024-06-04 04:40:32 +0530

    add functions and validations

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
 const container = document.querySelector("#container");
+const askBtn = document.querySelector("#askBtn");
 
+container.style.setProperty("--grid-size",16);
 //Create a sample grid
-for (let i = 0; i < 256 ; i++) {
-        const newCell = document.createElement("div");
-        newCell.classList.add("cell");
-        newCell.addEventListener("mouseover", () => {
-                newCell.style.background = "#DB8780";
-        });
-        container.appendChild(newCell);
-}
-
-let number = parseInt(prompt("Enter the number of squares per side for your grid (1-100)"));
-
-//recreate container
-container.innerHTML = "";
-container.style.setProperty("--grid--size", number);
-
-        for (let i = 0 ; i < number*number ; i++) {
-                const aCell = document.createElement("div");
-                aCell.classList.add("cell");
-                aCell.addEventListener("mouseover", () => {
-                        aCell.style.background = "#DB8780";
+function createGrid (num) {
+        for (let i = 0; i < num*num ; i++) {
+                const newCell = document.createElement("div");
+                newCell.classList.add("cell");
+                newCell.addEventListener("mouseover", () => {
+                        newCell.style.background = "#DB8780";
                 });
-                container.appendChild(aCell);
+                container.appendChild(newCell);
         }
+}
+createGrid(16);
+
+function changeGrid () {
+        let choice = parseInt(prompt("Enter the number of squares per side for your grid (1-100)"));
+        if (isNaN(choice)) {
+                alert("Please enter a number");
+                return;
+        }
+        if (choice < 1 || choice > 100) {
+                alert("Please enter a number between 1 and 100 (inclusive).");
+                return;
+       }
+        //recreate container
+        container.innerHTML = "";
+        container.style.setProperty("--grid-size", choice);
+
+                for (let i = 0 ; i < choice*choice ; i++) {
+                        const aCell = document.createElement("div");
+                        aCell.classList.add("cell");
+                        aCell.addEventListener("mouseover", () => {
+                                aCell.style.background = "#DB8780";
+                        });
+                        container.appendChild(aCell);
+                }
+}
+askBtn.addEventListener("click", changeGrid);
