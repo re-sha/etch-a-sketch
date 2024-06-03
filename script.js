2ade4b73c77239af8645ad9362038998c275994a
re-sha <163538829+re-sha@users.noreply.github.com>
2024-06-04 02:07:27 +0530

    add variable for each cell

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 const container = document.querySelector("#container");
+
+//Create a sample grid
 for (let i = 0; i < 256 ; i++) {
         const newCell = document.createElement("div");
         newCell.classList.add("cell");
         newCell.addEventListener("mouseover", () => {
-                newCell.style.background = "pink";
-        })
+                newCell.style.background = "#DB8780";
+        });
         container.appendChild(newCell);
 }
+
+let number = parseInt(prompt("Enter the number of squares per side for your grid (1-100)"));
+
+//recreate container
+container.innerHTML = "";
+container.style.setProperty("--grid--size", number);
+
+        for (let i = 0 ; i < number*number ; i++) {
+                const aCell = document.createElement("div");
+                aCell.classList.add("cell");
+                aCell.addEventListener("mouseover", () => {
+                        aCell.style.background = "#DB8780";
+                });
+                container.appendChild(aCell);
+        }
